8274f06db4c83050345e6fea91c69b1f13052905
Perukii <newertlll@gmail.com>
2020-04-09 19:49:09 +0900

    feat : テキストのフォント設定の追加

--- src/cairo_sparkle/objects/text.hpp
+++ src/cairo_sparkle/objects/text.hpp
@@ -2,7 +2,8 @@
 
 class casp_text : public casp_rect {
   public:
-    std::string text;
+    std::string text, font;
+    bool bold, italic;
     casp_rgb color_text;
 
     casp_text(std::string _text = "", double _x = 0, double _y = 0,
@@ -12,7 +13,6 @@ class casp_text : public casp_rect {
         setup_text(_text, _x, _y, _w, _h, casp_rgb_null, _pivx, _pivy);
     }
 
-
     void setup_text(std::string _text = "", double _x = 0, double _y = 0,
                     double _w = 0, double _h = 0,
                     casp_rgb _color_text = casp_rgb_null, double _pivx = 0,
@@ -20,7 +20,18 @@ class casp_text : public casp_rect {
         text = _text;
         color_text = _color_text;
         setup_rect(_x, _y, _w, _h, casp_rgb_null, _pivx, _pivy);
+        set_font("Sans", false, false);
     }
+
+    void set_font(std::string _font, bool _bold = false, bool _italic = false){
+        font = _font;
+        bold = _bold;
+        italic = _italic;
+    }
+
+    void set_font_file(std::string _font) { font = _font; }
+    void set_font_weight(bool _bold = false) { bold = _bold; }
+    void set_font_slant(bool _italic = false) { italic = _italic; }
 
     void draw_text() {
 
@@ -30,8 +41,9 @@ class casp_text : public casp_rect {
         cairo_set_source_rgba(surface->cr, color_text.r, color_text.g,
                               color_text.b, color_text.a);
 
-        cairo_select_font_face(surface->cr, "Sans", CAIRO_FONT_SLANT_NORMAL,
-                               CAIRO_FONT_WEIGHT_NORMAL);
+        cairo_select_font_face(surface->cr, font.c_str(),
+                                italic ? CAIRO_FONT_SLANT_ITALIC:CAIRO_FONT_SLANT_NORMAL,
+                                bold ? CAIRO_FONT_WEIGHT_BOLD:CAIRO_FONT_WEIGHT_NORMAL);
 
         cairo_text_extents_t extents;
 
