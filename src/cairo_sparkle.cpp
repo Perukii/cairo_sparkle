885c5f9754ba7fd9c74c3e51f4d80ddd871e0470
Perukii <newertlll@gmail.com>
2020-04-11 13:03:35 +0900

    other : windowツールキットをgtk3からgtkmm3に変更

--- src/cairo_sparkle.cpp
+++ src/cairo_sparkle.cpp
@@ -1,5 +1,8 @@
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#pragma once
+#include <gtkmm.h>
+//#include <gtk/gtk.h>
 
-#include <gtk/gtk.h>
 #include <iostream>
 #include <set>
 #include <string>
