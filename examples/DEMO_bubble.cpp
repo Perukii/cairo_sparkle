10c76fb9126ac1a44fa72d087db28bf91bacd4bd
Perukii <newertlll@gmail.com>
2020-04-12 15:38:57 +0900

    feat : 円オブジェクトのstrokeの追加

--- examples/DEMO_bubble.cpp
+++ examples/DEMO_bubble.cpp
@@ -10,9 +10,10 @@ int main(int _argc, char **_argv) {
     host.setup_host(500, 500, casp_translate_norm_w);
     host.set_surface(5);
     host.set_titlebar("BUBBLE");
-    circle.setup_circle(0.0, 0.0, 1.0, casp_rgb_white);
+    circle.setup_circle(0.0, 0.0, 1.0, casp_rgb_black);
     circle.set_surface(&host.surface[0]);
     subcic.setup_circle(2.0, 2.0, 2.0, casp_rgb_white);
+    subcic.setup_stroke(0.1, casp_rgb_black);
     subcic.set_surface(&host.surface[1]);
     host.run();
 }
@@ -22,8 +23,7 @@ void casp_main(){
 }
 
 void casp_draw(){
-    //host.surface->fill_white();
-
+    host.surface[0].fill();
     circle.draw_circle();
     subcic.draw_circle();
     
--- src/cairo_sparkle/objects/circle.hpp
+++ src/cairo_sparkle/objects/circle.hpp
@@ -1,12 +1,15 @@
 
-class casp_circle : public casp_object {
+class casp_circle : public casp_object, public casp_stroke {
 public:
     casp_xy<double> center, d_center;
     double rx, ry, d_rx, d_ry;
     casp_rgb color_circle;
+    bool circle = false;
     
     void setup_circle(double _x = 0, double _y = 0, double _r = 0,
                     casp_rgb _color_circle = casp_rgb_null){
+
+        circle = true;
         center = casp_xy<double>(_x, _y);
         rx = _r;
         ry = _r;
@@ -26,7 +29,13 @@ public:
         set_color(color_circle);
 
         cairo_arc(surface->cr, d_center.x, d_center.y, d_rx, 0.0, G_PI * 2.0);
-        cairo_fill_preserve(surface->cr);
+
+        if (stroke){
+            cairo_fill_preserve(surface->cr);
+            draw_stroke(this);
+        }
+        else if (circle)
+            cairo_fill(surface->cr);
 
     }
 
