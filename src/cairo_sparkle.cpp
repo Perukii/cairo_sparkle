32ca1bbe3dc2ee6bcbf2e3fa7dd12ba4712d4863
Perukii <newertlll@gmail.com>
2020-04-20 12:33:02 +0900

    refactor : c_rect

--- src/cairo_sparkle.cpp
+++ src/cairo_sparkle.cpp
@@ -1,7 +1,7 @@
 
-#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+//#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #pragma once
-#include <gtkmm.h>
+#include <gtk/gtk.h>
 #include <SDL2/SDL_mixer.h>
 
 #include <iostream>
--- src/cairo_sparkle/objects/rect.hpp
+++ src/cairo_sparkle/objects/rect.hpp
@@ -1,4 +1,63 @@
 
+
+class c_rect : public casp_object {
+private:
+    casp_xywh<double> xywh;
+    casp_xy<double> draw_pivot;
+    casp_rgb color_rect;
+    bool rect_enable;
+
+public:
+
+    template<class... Args> c_rect(Args...);
+
+    void setup_rect(casp_xywh<double>, casp_rgb, casp_xy<double>);
+
+    void enable_rect();
+    void disable_rect();
+
+    void draw_rect();
+
+};
+
+template<class... Args> c_rect::c_rect(Args... args){
+    setup_rect(args...);
+}
+
+void c_rect::setup_rect
+    (casp_xywh<double> _xywh = {0.0, 0.0, 0.0, 0.0},
+        casp_rgb _color_rect = casp_rgb_null,
+        casp_xy<double> _draw_pivot = {0.0, 0.0}){
+
+    enable_rect();
+    xywh = _xywh;
+    draw_pivot = _draw_pivot;
+    color_rect = _color_rect;
+
+    set_surface();
+}
+
+void c_rect::enable_rect() { rect_enable = true; }
+void c_rect::disable_rect() { rect_enable = false; }
+
+void c_rect::draw_rect() {
+
+    casp_xywh<double> d_xywh = surface->translate_xywh(xywh);
+    
+    cairo_rectangle(surface->cr,
+                    d_xywh.x - d_xywh.w * (draw_pivot.x + 1.0) * 0.5,
+                    d_xywh.y - d_xywh.h * (draw_pivot.y + 1.0) * 0.5,
+                    d_xywh.w, d_xywh.h);
+
+    set_color(color_rect);
+
+    if (rect_enable)
+        cairo_fill(surface->cr);
+    
+}
+
+// ==== //
+
 class casp_rect : public casp_object, public casp_stroke {
   public:
     casp_xy<double> pivot;
@@ -60,3 +119,5 @@ class casp_rect : public casp_object, public casp_stroke {
 
     
 };
+
+
